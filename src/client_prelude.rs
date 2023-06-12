3d118cdc3acf9589c4c68dc297d1d6f7dc9c4d40
Leonardo Forchini <git@lforchini.com>
2023-06-12 13:29:13 +0100

    Update version

--- src/client_prelude.rs
+++ src/client_prelude.rs
@@ -3,6 +3,7 @@ pub use crate::AsyncClient;
 pub use crate::Client;
 pub use crate::RestClient;
 
+pub use async_trait::async_trait;
 pub use bytes::Bytes;
 pub use http::{request::Builder as RequestBuilder, Error as HttpError, Response as HttpResponse};
 pub use url::{ParseError, Url};
